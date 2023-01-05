79d507c3ffb46d15c474d4be8b565cf59f08907f
samin7777 <111180605+samin7777@users.noreply.github.com>
2023-01-05 23:43:48 +0545

    Update serverside.py

---
+++ serverside.py
@@ -0,0 +1,22 @@
+import socket
+import struct
+
+def receive_covert_packet(sock, fragment_size=1480):
+    data = b''
+    while True:
+        packet, address = sock.recvfrom(fragment_size + 20)
+        fragment = packet[20:]
+        offset = struct.unpack("!H", packet[6:8])[0] & 0x1FFF
+        if offset == 0:
+            data = fragment
+        else:
+            data += fragment
+        if (struct.unpack("!H", packet[6:8])[0] & 0x2000) == 0:
+            break
+    return data
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+server_socket.bind(('0.0.0.0', 0))
+
+data = receive_covert_packet(server_socket)
+print(data.decode('utf-8'))
